d35a8cc7d089621f5356a02ccc332ca6b8aac24a
lautaorsi <lautaorsi@gmail.com>
2024-05-13 20:50:38 -0300

    Fixed several issues

--- index.js
+++ index.js
@@ -54,6 +54,7 @@ var player_count = 0
 var current_players = 0
 
 let rooms = {}
+let room;
 
 let player_id = {}
 
@@ -66,18 +67,17 @@ let player_id = {}
 //         2roomID: {}        
 //}
 
-//rooms[room] = {PLAYERS: [player, player, player]
+//room = {PLAYERS: [player, player, player]
 //               VIDEOS: [videoIndex, videoIndex, videoIndex]          (videoID = int)
 //}
 
 
 io.on('connection', (socket) => {
-  var video
+  var video, room_id
   current_players += 1;
   player_count += 1;
   console.log(`current players = ${current_players}`)
   console.log(`overall players = ${player_count}`)
-  var room = '';
   socket.emit('accepted'); 
 
 
@@ -94,27 +94,31 @@ io.on('connection', (socket) => {
     player_id[socket.id] = data.username;
     //join player to room
     socket.join(data.roomID) ;
-    room = data.roomID;
+    room_id = data.roomID;
     var color = getColor();
 
-    //if room exists, check valid username 
-    if(room in rooms){
+  
 
-      while(((rooms[room])['chosen_usernames']).includes(data.username)){
+    //if room exists, check valid username 
+    if(room_id in rooms){
+
+      while(((rooms[room_id])['chosen_usernames']).includes(data.username)){
         data.username = `${data.username}${rando(100)}`;
         
         player_id[socket.id] = data.username;
       }
-      socket.emit('rules', rooms[room][['Rules']]);
+      socket.emit('rules', room['Rules']);
 
-      rooms[room]['is_host'][socket.id] = false;
+      room = rooms[room_id]  
+
+      room['is_host'][socket.id] = false;
 
     }
     
     //if room doesn't exist, create new one
     else{
       //create room object within rooms dictionary
-      rooms[room] = {
+      rooms[room_id] = {
         'Players': [],
         'names': [],
         'chosen_usernames':[],
@@ -125,32 +129,37 @@ io.on('connection', (socket) => {
         'error_counter': 0,
         'is_host': {},
         'allow_guess': false,
+        'is_round_playing': false,
+        'Rules' : {},
       };
+      
+      room = rooms[room_id]  
 
       socket.emit('rules?',(rules) => {
-          rooms[room]['Rules'] = {};
-          rooms[room]['Rules']['mode'] = rules.mode;
-          rooms[room]['Rules']['rounds'] = (rules.rounds);
-          rooms[room]['Rules']['time'] = (rules.time);
-          rooms[room]['rounds'] = rules.rounds;
+          room['Rules']['mode'] = rules.mode;
+          room['Rules']['rounds'] = (rules.rounds);
+          room['Rules']['time'] = (rules.time);
+          room['rounds'] = rules.rounds;
       });  
-      (rooms[room])['is_host'][socket.id] = true; 
+      room['is_host'][socket.id] = true; 
     }
 
     //add player to room
-    (rooms[room])['Players'].push({username:data.username,color:color,socketID:socket.id});
-    ((rooms[room])['names']).push({socketID:socket.id,username:data.username});
-    ((rooms[room])['chosen_usernames']).push(data.username);
-    (rooms[room])['points'][socket.id] = {username: data.username,points:0};
+    room['Players'].push({username:data.username,color:color,socketID:socket.id});
+    (room['names']).push({socketID:socket.id,username:data.username});
+    (room['chosen_usernames']).push(data.username);
+    (room)['points'][socket.id] = {username: data.username,points:0};
     
 
-    io.emit('admin',({username:(rooms[room]['Players'])[0]['username'], socket:(rooms[room]['Players'])[0]['socketID']}));
+    io.emit('admin',({username:(room['Players'])[0]['username'], socket:(room['Players'])[0]['socketID']}));
     //update player list to all players in room
-    io.to(room).emit('users', rooms[room]['Players'])
+    io.to(room_id).emit('users', room['Players'])
+    console.log(room['Players'])
     callback({
       col: color,
       name: data.username
     });
+    console.log(rooms)
   }) 
 
   //user disconnection sequence
@@ -159,36 +168,36 @@ io.on('connection', (socket) => {
     current_players -= 1;
     //when user disconnects, remove him from the player list
     try{
-      var room_players_amnt = (rooms[room]['Players']).length
+      var room_players_amnt = (room['Players']).length
       for(var i = 0 ; i < room_players_amnt; i++){
-        if((rooms[room]['Players'])[i]['socketID'] == [socket.id]){
+        if((room['Players'])[i]['socketID'] == [socket.id]){
   
 
 
 
           //remove him from room list
-          (rooms[room]['names']).splice(i, 1);
-          (rooms[room]['Players']).splice(i, 1);
-          (rooms[room]['chosen_usernames']).splice(i, 1);
-          delete rooms[room]['points'][socket.id];
+          (room['names']).splice(i, 1);
+          (room['Players']).splice(i, 1);
+          (room['chosen_usernames']).splice(i, 1);
+          delete room['points'][socket.id];
 
 
           //if admin left, give admin to next player
-          if((rooms[room])['is_host'][socket.id] && rooms[room]['Players'].length != 0){
-            delete rooms[room]['is_host'][socket.id];
-            io.emit('admin',({username:(rooms[room]['Players'])[0]['username'], socket:(rooms[room]['Players'])[0]['socketID']}));
-            var new_hst = (rooms[room]['Players'])[0]['socketID'];
-            rooms[room]['is_host'][new_hst] = true;            
+          if((room)['is_host'][socket.id] && room['Players'].length != 0){
+            delete room['is_host'][socket.id];
+            io.emit('admin',({username:(room['Players'])[0]['username'], socket:(room['Players'])[0]['socketID']}));
+            var new_hst = (room['Players'])[0]['socketID'];
+            room['is_host'][new_hst] = true;            
           }
 
 
           //if room is empty, delete it
-          if(rooms[room]['Players'].length == 0){
-            delete rooms[room];
+          if(room['Players'].length == 0){
+            delete room;
           }
           else{
           //emit disconnection event with player's username and new player list
-          io.to(room).emit('player_disconnected', ({username : player_id[socket.id], new_list: (rooms[room]['Players'])}));
+          io.to(room_id).emit('player_disconnected', ({username : player_id[socket.id], new_list: (room['Players'])}));
           }
           break
         }
@@ -211,21 +220,22 @@ io.on('connection', (socket) => {
   //game start sequence
   socket.on('start', () => {
     //only host can start the match (person in first position)
-    if(socket.id == rooms[room]['Players'][0].socketID){
+    if(socket.id == room['Players'][0].socketID){
       
       //to start, minimum of 2 players required
-      if(rooms[room]['Players'].length >= 2){
+      if(room['Players'].length >= 2){
         video = rando(videos_qty)
 
         //add video id to lobby's video list
-        rooms[room]['VIDEOS'] = []
-        rooms[room]['VIDEOS'].push(video)
+        room['VIDEOS'] = []
+        room['VIDEOS'].push(video)
 
         //fire game start event with video ID as param
-        io.to(room).emit('game start', video)
-        rooms[room]['allow_guess'] = true
+        io.to(room_id).emit('game start', video)
+        room['allow_guess'] = true
       }
     }
+    room.is_round_playing = true
   })
 
 
@@ -235,28 +245,29 @@ io.on('connection', (socket) => {
   socket.on('user_guessed', (data) => {
     console.log(data)
     //increase guess counter
-    rooms[room].guesses += 1
+    room.guesses += 1
     
     //add guess information to room array for later use
-    rooms[room].guess_data[data.username] = {coords1:data.coords1,coords2:data.coords2,points:data.points,color:data.color}
+    room.guess_data[data.username] = {coords1:data.coords1,coords2:data.coords2,points:data.points,color:data.color}
     
     //add points to user
-    rooms[room]['points'][socket.id].points += parseInt(data.points) 
+    room['points'][socket.id].points += parseInt(data.points) 
 
     //if user guessed on diff coords than 0,0 then they guessed, so notification should show 
     if(data.coords1 != 0 && data.coords2 != 0){
-    io.except(socket.id).to(room).emit('player_guessed', {username:data.username,distance:((data.distance).toFixed(0))})
+    io.except(socket.id).room_id.emit('player_guessed', {username:data.username,distance:((data.distance).toFixed(0))})
     }
     //if the guess amount equals the amount of players then all guessed event is triggered
-    if (rooms[room].guesses == rooms[room]['Players'].length){
+    if (room.guesses == room['Players'].length){
+      room.is_round_playing = false
 
       //fire all guessed event, passing guess data from all players
-      io.to(room).emit('all_guessed',rooms[room].guess_data)
-      rooms[room]['allow_guess'] = false
+      io.to(room_id).emit('all_guessed',room.guess_data)
+      room['allow_guess'] = false
       
       //restart guess counter and data
-      rooms[room].guess_data = {}
-      rooms[room].guesses = 0
+      room.guess_data = {}
+      room.guesses = 0
     }
 
   })
@@ -264,21 +275,21 @@ io.on('connection', (socket) => {
   //video error handling
   socket.on('video_error', () => {
     //increase error counter
-    (rooms[room]).error_counter += 1
+    (room).error_counter += 1
 
     //only aknowledge error if more than half the players run into error
-    if((rooms[room]).error_counter >= (rooms[room]['Players'].length/2)){
+    if((room).error_counter >= (room['Players'].length/2)){
 
       //choose new rand video upon aknowledgment
-      while(rooms[room]['VIDEOS'].includes(video)){
+      while(room['VIDEOS'].includes(video)){
         video =  rando(videos_qty)
       }
 
       //once new video is found, restart error counters, add new vid index to lobby's video list and fire replacing video event
-      rooms[room]['VIDEOS'].push(video)
-      io.to(room).emit('replace_vid', video)
-      rooms[room].players_ready = 0
-      rooms[room].error_counter = 0
+      room['VIDEOS'].push(video)
+      io.to(room_id).emit('replace_vid', video)
+      room.players_ready = 0
+      room.error_counter = 0
     }
   })
 
@@ -288,20 +299,20 @@ io.on('connection', (socket) => {
   socket.on('new_round', (callback) => {
 
     //validate user is admin
-    if(rooms[room]['is_host'][socket.id]){
-
-      rooms[room]['allow_guess'] = true
+    if(room['is_host'][socket.id]){
+      room.is_round_playing = true
+      room['allow_guess'] = true
       //if round counter is less than max rounds set, start new round 
-      if(rooms[room]['current_round'] < parseInt(rooms[room]['rounds'])){
-        console.log(`round: ${rooms[room]['current_round']}/${parseInt(rooms[room]['rounds'])}`)
+      if(room['current_round'] < parseInt(room['rounds'])){
+        console.log(`round: ${room['current_round']}/${parseInt(room['rounds'])}`)
         while(true){
           video = rando(videos_qty)
-          if(validateVideo(rooms[room]['VIDEOS'],video)){
-            rooms[room]['current_round'] += 1
-            rooms[room]['VIDEOS'].push(video)
-            io.to(room).emit('new_vid', video)
-            rooms[room].players_ready = 0
-            console.log(rooms[room][`VIDEOS`])
+          if(validateVideo(room['VIDEOS'],video)){
+            room['current_round'] += 1
+            room['VIDEOS'].push(video)
+            io.to(room_id).emit('new_vid', video)
+            room.players_ready = 0
+            console.log(room[`VIDEOS`])
             break
           }
         }
@@ -309,7 +320,7 @@ io.on('connection', (socket) => {
       else{
         console.log('ending game')
         //if all rounds have been played, end match
-        io.to(room).emit('end', rooms[room].points)
+        io.to(room_id).emit('end', room.points)
       }
     }
     
